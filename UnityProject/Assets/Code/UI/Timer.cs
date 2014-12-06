fb41665e284e5f748b3412451d6d92c90394968d
Casey Clyde <caseysclyde@gmail.com>
2014-12-05 23:40:04 -0800

    timer

--- UnityProject/Assets/Code/UI/Timer.cs
+++ UnityProject/Assets/Code/UI/Timer.cs
@@ -5,6 +5,7 @@ using UnityEngine.UI;
 public class Timer : MonoBehaviour {
 
 	private float startTime = 0;
+	public Text timerText;
 
 	// Use this for initialization
 	void Start () {
@@ -23,10 +24,8 @@ public class Timer : MonoBehaviour {
 		float seconds = guiTime % 60;
 		float fraction = (guiTime * 100) % 100;
 
-		if (gameObject.guiText != null) {
-			gameObject.guiText.text = string.Format ("{0:00}:{1:00}:{2:00}", minutes, seconds, fraction);
-		}
+		timerText.text = string.Format ("{0:00}:{1:00}:{2:00}", minutes, seconds, fraction);
 
-		gameObject.guiText.text = "asss";
+		//gameObject.guiText.text = "asss";
 	}
 }
