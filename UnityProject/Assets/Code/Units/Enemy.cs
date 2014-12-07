473cb3f01fd78098ea4600c7d005febbdb43883a
Corey Busta Wolff <jesterswilde@gmail.com>
2014-12-06 20:49:07 -0800

    Worker AI...Works

--- UnityProject/Assets/Code/Units/Enemy.cs
+++ UnityProject/Assets/Code/Units/Enemy.cs
@@ -3,6 +3,18 @@ using System.Collections;
 
 public class Enemy : Unit {
 
+	Worker _presentThreat;
+
+	public void GotShot(Worker _shooter, int damage){
+		_health -= damage; 
+		if (_health <= 0) {
+			Died();	
+		}
+		if (_presentThreat == null) {
+			_presentThreat = _shooter; 	
+		}
+	}
+
 	protected override void Died (){
 		_manager.Died (this);
 	}
