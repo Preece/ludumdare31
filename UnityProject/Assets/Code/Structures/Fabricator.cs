642857b65155c0376a5fd3572464616e9a5f3999
Casey Clyde <caseysclyde@gmail.com>
2014-12-07 16:39:05 -0800

    stufffffxxxxxxxbustaxxx

--- UnityProject/Assets/Code/Structures/Fabricator.cs
+++ UnityProject/Assets/Code/Structures/Fabricator.cs
@@ -1,10 +1,42 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Fabricator : ResourceNode {
 	
+	public double rawPerPart = 3.0;
+	public double fuelPerPart = 2.0;
+
 	//how this structure will move its jank each tick
 	public override void Process() {
-		
+		//convert raw stuff into fuel
+		if(raw >= rawPerPart && fuel >= fuelPerPart) {
+			raw -= rawPerPart;
+			fuel -= fuelPerPart;
+			parts++;
+		}
+
+		if(parts <= 0) return;
+
+		double outputAmt = output;
+
+		if(parts < outputAmt) outputAmt = parts;
+
+		List<ResourceNode> activeFeedees = new List<ResourceNode>();
+
+		for(int i = 0; i < feedees.Count; i++) {
+			if(!feedees[i].AtCapacity()) {
+				activeFeedees.Add(feedees[i]);
+			}
+		}
+
+		if(activeFeedees.Count == 0) return;
+
+		for(int i = 0; i < activeFeedees.Count; i++) {
+			double amttt = outputAmt;
+			amttt /= (double)activeFeedees.Count;
+			activeFeedees[i].AddParts(amttt);
+			parts -= amttt;
+		}
 	}
 }
